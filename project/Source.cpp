d1699682257eb2afb61aef634f58e50fe2e727a5
abhijeetsinghtomar <abhijeetsinghemail@gmail.com>
2019-03-25 22:43:15 +0530

    Add black & white program with instructions

--- project/Source.cpp
+++ project/Source.cpp
@@ -1,241 +1,267 @@
-
-////////////////////////////////////////////////////////////
-// Headers
-////////////////////////////////////////////////////////////
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <cmath>
-#include <ctime>
-#include <cstdlib>
 
+//#include <GL/gl.h>
+#include <GL/glut.h>
+#include <GL/glu.h>
 
-////////////////////////////////////////////////////////////
-/// Entry point of application
-///
-/// \return Application exit code
-///
-////////////////////////////////////////////////////////////
-int main()
-{
-	std::srand(static_cast<unsigned int>(std::time(NULL)));
+static int animationPeriod = 4;
+static int isAnimate = 0;
 
-	// Define some constants
-	const float pi = 3.14159f;
-	const int gameWidth = 800;
-	const int gameHeight = 600;
-	sf::Vector2f paddleSize(25, 100);
-	float ballRadius = 10.f;
+const int fact = 3;
+const int x = 80;
+const double DEG2RAD = 3.1415926535897932384 / 180;
 
-	// Create the window of the application
-	sf::RenderWindow window(sf::VideoMode(gameWidth, gameHeight, 32), "SFML Pong",
-		sf::Style::Titlebar | sf::Style::Close);
-	window.setVerticalSyncEnabled(true);
+static double w = 200;
+static int flag = 0;
+static int walk = 0;
+static int x_ = 2500;
+using namespace std;
 
-	// Load the sounds used in the game
-	sf::SoundBuffer ballSoundBuffer;
-	if (!ballSoundBuffer.loadFromFile("resources/ball.wav"))
-		return EXIT_FAILURE;
-	sf::Sound ballSound(ballSoundBuffer);
+void animate(int value) {
+	if (isAnimate) {
+		glutPostRedisplay();
+		glutTimerFunc(animationPeriod, animate, 1);
+	}
+}
 
-	// Create the left paddle
-	sf::RectangleShape leftPaddle;
-	leftPaddle.setSize(paddleSize - sf::Vector2f(3, 3));
-	leftPaddle.setOutlineThickness(3);
-	leftPaddle.setOutlineColor(sf::Color::Black);
-	leftPaddle.setFillColor(sf::Color(100, 100, 200));
-	leftPaddle.setOrigin(paddleSize / 2.f);
-
-	// Create the right paddle
-	sf::RectangleShape rightPaddle;
-	rightPaddle.setSize(paddleSize - sf::Vector2f(3, 3));
-	rightPaddle.setOutlineThickness(3);
-	rightPaddle.setOutlineColor(sf::Color::Black);
-	rightPaddle.setFillColor(sf::Color(200, 100, 100));
-	rightPaddle.setOrigin(paddleSize / 2.f);
-
-	// Create the ball
-	sf::CircleShape ball;
-	ball.setRadius(ballRadius - 3);
-	ball.setOutlineThickness(3);
-	ball.setOutlineColor(sf::Color::Black);
-	ball.setFillColor(sf::Color::White);
-	ball.setOrigin(ballRadius / 2, ballRadius / 2);
-
-	// Load the text font
-	sf::Font font;
-	if (!font.loadFromFile("resources/sansation.ttf"))
-		return EXIT_FAILURE;
-
-	// Initialize the pause message
-	sf::Text pauseMessage;
-	pauseMessage.setFont(font);
-	pauseMessage.setCharacterSize(40);
-	pauseMessage.setPosition(170.f, 150.f);
-	pauseMessage.setFillColor(sf::Color::White);
-	pauseMessage.setString("Welcome to SFML pong!\nPress space to start the game");
-
-	// Define the paddles properties
-	sf::Clock AITimer;
-	const sf::Time AITime = sf::seconds(0.1f);
-	const float paddleSpeed = 400.f;
-	float rightPaddleSpeed = 0.f;
-	const float ballSpeed = 400.f;
-	float ballAngle = 0.f; // to be changed later
-
-	sf::Clock clock;
-	bool isPlaying = false;
-	while (window.isOpen())
-	{
-		// Handle events
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			// Window closed or escape key pressed: exit
-			if ((event.type == sf::Event::Closed) ||
-				((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)))
-			{
-				window.close();
-				break;
-			}
-
-			// Space key pressed: play
-			if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Space))
-			{
-				if (!isPlaying)
-				{
-					// (re)start the game
-					isPlaying = true;
-					clock.restart();
-
-					// Reset the position of the paddles and ball
-					leftPaddle.setPosition(10 + paddleSize.x / 2, gameHeight / 2);
-					rightPaddle.setPosition(gameWidth - 10 - paddleSize.x / 2, gameHeight / 2);
-					ball.setPosition(gameWidth / 2, gameHeight / 2);
-
-					// Reset the ball angle
-					do
-					{
-						// Make sure the ball initial angle is not too much vertical
-						ballAngle = (std::rand() % 360) * 2 * pi / 360;
-					} while (std::abs(std::cos(ballAngle)) < 0.7f);
-				}
-			}
+void keyInput(unsigned char key, int x, int y) {
+	switch (key) {
+	case 27:
+		exit(0);
+	case ' ':
+		if (isAnimate) isAnimate = 0;
+		else {
+			isAnimate = 1;
+			animate(1);
 		}
+		break;
+	}
+}
+bool collision(double len) {
+	if (abs(157 + x - (x_ + x + 50)) <= 100 + x) {
+		if (5 * fact + w <= 650 * len)return 1;
+		return 0;
+	}
+	return 0;
+}
 
-		if (isPlaying)
-		{
-			float deltaTime = clock.restart().asSeconds();
+void specialKeyInput(int key, int x, int y) {
+	if (key == GLUT_KEY_UP && flag == 0 && w <= 200.0) {
+		flag = 1;
+	}
+	glutPostRedisplay();
+}
 
-			// Move the player's paddle
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) &&
-				(leftPaddle.getPosition().y - paddleSize.y / 2 > 5.f))
-			{
-				leftPaddle.move(0.f, -paddleSpeed * deltaTime);
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) &&
-				(leftPaddle.getPosition().y + paddleSize.y / 2 < gameHeight - 5.f))
-			{
-				leftPaddle.move(0.f, paddleSpeed * deltaTime);
-			}
-
-			// Move the computer's paddle
-			if (((rightPaddleSpeed < 0.f) && (rightPaddle.getPosition().y - paddleSize.y / 2 > 5.f)) ||
-				((rightPaddleSpeed > 0.f) && (rightPaddle.getPosition().y + paddleSize.y / 2 < gameHeight - 5.f)))
-			{
-				rightPaddle.move(0.f, rightPaddleSpeed * deltaTime);
-			}
-
-			// Update the computer's paddle direction according to the ball position
-			if (AITimer.getElapsedTime() > AITime)
-			{
-				AITimer.restart();
-				if (ball.getPosition().y + ballRadius > rightPaddle.getPosition().y + paddleSize.y / 2)
-					rightPaddleSpeed = paddleSpeed;
-				else if (ball.getPosition().y - ballRadius < rightPaddle.getPosition().y - paddleSize.y / 2)
-					rightPaddleSpeed = -paddleSpeed;
-				else
-					rightPaddleSpeed = 0.f;
-			}
-
-			// Move the ball
-			float factor = ballSpeed * deltaTime;
-			ball.move(std::cos(ballAngle) * factor, std::sin(ballAngle) * factor);
-
-			// Check collisions between the ball and the screen
-			if (ball.getPosition().x - ballRadius < 0.f)
-			{
-				isPlaying = false;
-				pauseMessage.setString("You lost!\nPress space to restart or\nescape to exit");
-			}
-			if (ball.getPosition().x + ballRadius > gameWidth)
-			{
-				isPlaying = false;
-				pauseMessage.setString("You won!\nPress space to restart or\nescape to exit");
-			}
-			if (ball.getPosition().y - ballRadius < 0.f)
-			{
-				ballSound.play();
-				ballAngle = -ballAngle;
-				ball.setPosition(ball.getPosition().x, ballRadius + 0.1f);
-			}
-			if (ball.getPosition().y + ballRadius > gameHeight)
-			{
-				ballSound.play();
-				ballAngle = -ballAngle;
-				ball.setPosition(ball.getPosition().x, gameHeight - ballRadius - 0.1f);
-			}
-
-			// Check the collisions between the ball and the paddles
-			// Left Paddle
-			if (ball.getPosition().x - ballRadius < leftPaddle.getPosition().x + paddleSize.x / 2 &&
-				ball.getPosition().x - ballRadius > leftPaddle.getPosition().x &&
-				ball.getPosition().y + ballRadius >= leftPaddle.getPosition().y - paddleSize.y / 2 &&
-				ball.getPosition().y - ballRadius <= leftPaddle.getPosition().y + paddleSize.y / 2)
-			{
-				if (ball.getPosition().y > leftPaddle.getPosition().y)
-					ballAngle = pi - ballAngle + (std::rand() % 20) * pi / 180;
-				else
-					ballAngle = pi - ballAngle - (std::rand() % 20) * pi / 180;
-
-				ballSound.play();
-				ball.setPosition(leftPaddle.getPosition().x + ballRadius + paddleSize.x / 2 + 0.1f, ball.getPosition().y);
-			}
-
-			// Right Paddle
-			if (ball.getPosition().x + ballRadius > rightPaddle.getPosition().x - paddleSize.x / 2 &&
-				ball.getPosition().x + ballRadius < rightPaddle.getPosition().x &&
-				ball.getPosition().y + ballRadius >= rightPaddle.getPosition().y - paddleSize.y / 2 &&
-				ball.getPosition().y - ballRadius <= rightPaddle.getPosition().y + paddleSize.y / 2)
-			{
-				if (ball.getPosition().y > rightPaddle.getPosition().y)
-					ballAngle = pi - ballAngle + (std::rand() % 20) * pi / 180;
-				else
-					ballAngle = pi - ballAngle - (std::rand() % 20) * pi / 180;
-
-				ballSound.play();
-				ball.setPosition(rightPaddle.getPosition().x - ballRadius - paddleSize.x / 2 - 0.1f, ball.getPosition().y);
-			}
+void draw_circle(double theta, double inner_radius, double outer_radius, int x, int y, int sin_sign = 1, int cos_sign = 1) {
+	glBegin(GL_POINTS);
+	glColor3f(0.0 / 255.0, 0.0 / 255.0, 0.0 / 255.0);
+	for (double r = outer_radius; r >= inner_radius; r -= 3.0) {
+		for (double i = 0; i < theta; i++) {
+			double degInRad = i * DEG2RAD;
+			glVertex2f(cos_sign * cos(degInRad) * r + x, sin_sign * sin(degInRad) * r + y);
 		}
+	}
+	glEnd();
+}
 
-		// Clear the window
-		window.clear(sf::Color(50, 200, 50));
+void generate_tree(int x_, double len) {
+	int x = 30;
+	glColor3f((0) / 255.0, (0) / 255.0, (0) / 255.0);
+	glBegin(GL_POLYGON);
+	glVertex2f(x_, 250 * len);
+	glVertex2f(x_ + x, 250 * len);
+	glVertex2f(x_ + x, 650 * len);
+	glVertex2f(x_, 650 * len);
+	glEnd();
 
-		if (isPlaying)
-		{
-			// Draw the paddles and the ball
-			window.draw(leftPaddle);
-			window.draw(rightPaddle);
-			window.draw(ball);
+	draw_circle(180.0, 0.0, x / 2, x_ + x / 2, 650 * len);
+
+	glBegin(GL_POLYGON);
+	glVertex2f(x_ + x + 25, 400 * len);
+	glVertex2f(x_ + x + 50, 400 * len);
+	glVertex2f(x_ + x + 50, 600 * len);
+	glVertex2f(x_ + x + 25, 600 * len);
+	glEnd();
+
+	draw_circle(180.0, 0.0, 25.0 / 2, x_ + x + 75.0 / 2, 600 * len);
+
+	glBegin(GL_POLYGON);
+	glVertex2f(x_ - 25, 400 * len);
+	glVertex2f(x_ - 50, 400 * len);
+	glVertex2f(x_ - 50, 600 * len);
+	glVertex2f(x_ - 25, 600 * len);
+	glEnd();
+
+	draw_circle(180.0, 0.0, 25.0 / 2, x_ - 75.0 / 2, 600 * len);
+	draw_circle(90.0, 25, 50, x_ + x, 400 * len, -1);
+	draw_circle(90.0, 25, 50, x_, 400 * len, -1, -1);
+}
+
+void reset() {
+	w = 200;
+	flag = 0;
+	walk = 0;
+	x_ = 2500;
+	animationPeriod = 4;
+	isAnimate = 0;
+}
+
+void render(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glPointSize(2);
+	glBegin(GL_POINTS);
+	glColor3f((0) / 255.0, (0) / 255.0, (0) / 255.0);
+	for (int i = 0; i < 100; i++) {
+		glVertex2f(rand() % 2000, 200);
+		glVertex2f((rand() + 31) % 2000, 150);
+	}
+	glEnd();
+
+	generate_tree(x_, 1.0);
+
+
+	if (x_ >= 0)
+		x_ -= 5;
+	else {
+		x_ = 2000 + rand() % 400;
+	}
+	glLineWidth(2);
+	glBegin(GL_LINES);
+	glColor3f((0) / 255.0, (0) / 255.0, (0) / 255.0);
+	glVertex2f(0, 250);
+	glVertex2f(2000, 250);
+	glEnd();
+
+	glLineWidth(10);
+	glBegin(GL_LINES);
+	glColor3f(0 / 255.0, 0 / 255.0, 0 / 255.0);
+
+	glVertex2f(10 + x, 75 * fact + w);
+	glVertex2f(10 + x, 45 * fact + w);
+	glVertex2f(15 + x, 65 * fact + w);
+	glVertex2f(15 + x, 40 * fact + w);
+	glVertex2f(20 + x, 60 * fact + w);
+	glVertex2f(20 + x, 35 * fact + w);
+	glVertex2f(25 + x, 55 * fact + w);
+	glVertex2f(25 + x, 35 * fact + w);
+	glVertex2f(30 + x, 55 * fact + w);
+	glVertex2f(30 + x, 35 * fact + w);
+	glVertex2f(35 + x, 55 * fact + w);
+	glVertex2f(35 + x, 25 * fact + w);
+	glVertex2f(40 + x, 60 * fact + w);
+	glVertex2f(40 + x, 5 * fact + w - walk);
+	glVertex2f(45 + x, 65 * fact + w);
+	glVertex2f(45 + x, 15 * fact + w);
+	glVertex2f(45 + x, 10 * fact + w - walk);
+	glVertex2f(45 + x, 5 * fact + w - walk);
+	glVertex2f(50 + x, 10 * fact + w - walk);
+	glVertex2f(50 + x, 5 * fact + w - walk);
+	glVertex2f(55 + x, 10 * fact + w - walk);
+	glVertex2f(55 + x, 5 * fact + w - walk);
+	glVertex2f(50 + x, 65 * fact + w);
+	glVertex2f(50 + x, 20 * fact + w);
+	glVertex2f(55 + x, 70 * fact + w);
+	glVertex2f(55 + x, 25 * fact + w);
+	glVertex2f(63 + x, 75 * fact + w);
+	glVertex2f(63 + x, 20 * fact + w);
+	glVertex2f(70 + x, 115 * fact + w);
+	glVertex2f(70 + x, 5 * fact + w + walk);
+	glVertex2f(78 + x, 120 * fact + w);
+	glVertex2f(78 + x, 25 * fact + w);
+	glVertex2f(78 + x, 10 * fact + w + walk);
+	glVertex2f(78 + x, 5 * fact + w + walk);
+	glVertex2f(85 + x, 10 * fact + w + walk);
+	glVertex2f(85 + x, 5 * fact + w + walk);
+	glVertex2f(87 + x, 120 * fact + w);
+	glVertex2f(87 + x, 115 * fact + w);
+	glVertex2f(87 + x, 110 * fact + w);
+	glVertex2f(87 + x, 30 * fact + w);
+	glVertex2f(95 + x, 120 * fact + w);
+	glVertex2f(95 + x, 35 * fact + w);
+	glVertex2f(103 + x, 120 * fact + w);
+	glVertex2f(103 + x, 75 * fact + w);
+	glVertex2f(103 + x, 65 * fact + w);
+	glVertex2f(103 + x, 60 * fact + w);
+	glVertex2f(110 + x, 65 * fact + w);
+	glVertex2f(110 + x, 60 * fact + w);
+	glVertex2f(118 + x, 65 * fact + w);
+	glVertex2f(118 + x, 55 * fact + w);
+	glVertex2f(112 + x, 120 * fact + w);
+	glVertex2f(112 + x, 85 * fact + w);
+	glVertex2f(112 + x, 80 * fact + w);
+	glVertex2f(112 + x, 75 * fact + w);
+	glVertex2f(120 + x, 120 * fact + w);
+	glVertex2f(120 + x, 85 * fact + w);
+	glVertex2f(120 + x, 80 * fact + w);
+	glVertex2f(120 + x, 75 * fact + w);
+	glVertex2f(126 + x, 120 * fact + w);
+	glVertex2f(126 + x, 85 * fact + w);
+	glVertex2f(126 + x, 80 * fact + w);
+	glVertex2f(126 + x, 75 * fact + w);
+	glVertex2f(135 + x, 120 * fact + w);
+	glVertex2f(135 + x, 85 * fact + w);
+	glVertex2f(135 + x, 80 * fact + w);
+	glVertex2f(135 + x, 75 * fact + w);
+	glVertex2f(142 + x, 120 * fact + w);
+	glVertex2f(142 + x, 85 * fact + w);
+	glVertex2f(150 + x, 120 * fact + w);
+	glVertex2f(150 + x, 85 * fact + w);
+	glVertex2f(157 + x, 115 * fact + w);
+	glVertex2f(157 + x, 85 * fact + w);
+
+	glEnd();
+
+	if (collision(1.0)) {
+		reset();
+	}
+	if (w <= 200) {
+		if (walk == -20)
+			walk = 20;
+		else {
+			walk = -20;
 		}
-		else
-		{
-			// Draw the pause message
-			window.draw(pauseMessage);
-		}
-
-		// Display things on screen
-		window.display();
+	}
+	else {
+		walk = 0;
 	}
 
-	return EXIT_SUCCESS;
+	if (flag == 1) {
+		if (w <= 1000) {
+			w = w + 8;
+		}
+		else {
+			flag = 0;
+		}
+	}
+	else if (w >= 200)
+		w = w - 8;
+	glFlush();
+}
+
+
+void setup(void) {
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	gluOrtho2D(0.0, 2000, 0.0, 2000);
+}
+
+int main(int argc, char** argv) {
+	srand(time(NULL));
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowSize(1230, 650);
+	glutInitWindowPosition(50, 0);
+	glutCreateWindow("Dinosaur!!");
+	setup();
+	glutDisplayFunc(render);
+
+	glutKeyboardFunc(keyInput);
+	glutSpecialFunc(specialKeyInput);
+
+	glutMainLoop();
 }
